a80c49f6a64d0776c6db9a89ee7c0334a2dcabed
MrBonini <43351923+mrbonini@users.noreply.github.com>
2019-10-24 20:28:16 -0300

    builded project

---
+++ Question 3/build/precache-manifest.a0bb8a3b7502216b553765e2d322047f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec86d217083966ad1655ef495434151e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a3dd19fc2f0b5cc8dbb6",
+    "url": "/static/css/main.3616c21b.chunk.css"
+  },
+  {
+    "revision": "041f37cea3b7129bc8c4",
+    "url": "/static/js/2.87cb55c0.chunk.js"
+  },
+  {
+    "revision": "a3dd19fc2f0b5cc8dbb6",
+    "url": "/static/js/main.5954327c.chunk.js"
+  },
+  {
+    "revision": "88918994ce998c066d1e",
+    "url": "/static/js/runtime-main.cf98dd5f.js"
+  },
+  {
+    "revision": "3b80cb1c32d9e971eae57de35fd60e72",
+    "url": "/static/media/background.3b80cb1c.jpg"
+  }
+]);
